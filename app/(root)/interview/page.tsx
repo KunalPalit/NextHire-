c017207b678e9d6a16fe9a52ec4a6cf5f6e22cc7
KunalPalit <kunalpalit006@gmail.com>
2025-04-23 14:04:48 +0530

    Vapi workflow setup and agent integartion into the application.

--- app/(root)/interview/page.tsx
+++ app/(root)/interview/page.tsx
@@ -1,10 +1,17 @@
 import Agent from "@/components/Agent";
+import { getCurrentUser } from "@/lib/actions/signup-actions";
 
-export default function InterviewPage() {
+export default async function InterviewPage() {
+  const user = await getCurrentUser();
   return (
     <>
       <h3>Interview Generation</h3>
-      <Agent userName="You" userId="user1" type="generate" />
+      {/* <Agent userName={user?.name} userId={user?.id} type="generate" /> */}
+      <Agent
+        userName={user?.name ?? "Guest"}
+        userId={user?.id}
+        type="generate"
+      />
     </>
   );
 }
